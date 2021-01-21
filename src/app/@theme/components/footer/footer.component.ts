1ef2ce34bc891eeb417f7b2485851857ddb9d4a3
Grzegorz Siewruk <gsiewruk+gitlab@gmail.com>
2021-01-21 14:28:14 +0100

    Added footer info - CERT OrangePL contribution with logo

--- src/app/@theme/components/footer/footer.component.ts
+++ src/app/@theme/components/footer/footer.component.ts
@@ -4,6 +4,7 @@ import { Component } from '@angular/core';
   selector: 'ngx-footer',
   styleUrls: ['./footer.component.scss'],
   template: `
+    Shared by <a href="https://cert.orange.pl">CERT Orange Poland</a> <img src="../../../../assets/images/logo_orange.png" height="50px">
   `,
 })
 export class FooterComponent {
