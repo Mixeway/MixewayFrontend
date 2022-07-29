89d033df576d4c1c29ff058af42022866fdd4a9d
G S <gs@gs-japko.local>
2022-07-29 11:44:25 +0200

    merging projects

--- src/app/pages/mixer-dashboard/project-table/project-table.component.ts
+++ src/app/pages/mixer-dashboard/project-table/project-table.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, TemplateRef} from '@angular/core';
 import { MixerProgresComponent } from '../../extra-components/mixer-progres/mixer-progres.component';
 import {DashboardService} from '../../../@core/service/DashboardService';
 import {Project} from '../../../@core/Model/Project';
@@ -7,6 +7,10 @@ import {CookieService} from 'ngx-cookie-service';
 import {Router} from '@angular/router';
 import {DashboardConstants} from '../../../@core/constants/DashboardConstants';
 import {VulnmanageColorComponent} from '../../extra-components/vulnmanage-color.component';
+import {NbDialogService} from '@nebular/theme';
+import {ej} from '@syncfusion/ej2-data/dist/global';
+import data = ej.data;
+import {FormBuilder, Validators} from '@angular/forms';
 
 
 @Component({
@@ -21,11 +25,16 @@ export class ProjectTableComponent {
   settings: any;
   loading: boolean = true;
   constants: DashboardConstants = new DashboardConstants();
+  mergeProjectsForm;
   constructor( private dashboardService: DashboardService, private toast: Toast, private cookieService: CookieService,
-               private router: Router) {
+               private router: Router, private dialogService: NbDialogService, private formBuilder: FormBuilder) {
     this.role = this.cookieService.get('role');
     this.loadSettings();
     this.getProjects();
+    this.mergeProjectsForm = this.formBuilder.group({
+      source: [0, Validators.required],
+      destination: [0, Validators.required],
+    });
   }
   loadSettings() {
     this.settings = {
@@ -33,6 +42,12 @@ export class ProjectTableComponent {
         add: true,
         edit: true,
         delete: true,
+        custom: [
+          {
+            name: 'swap',
+            title: '<i class="nb-arrow-retweet"></i>',
+          },
+        ],
       },
       add: {
         title: this.constants.DASHBOARD_TABLE_ADD,
@@ -110,14 +125,14 @@ export class ProjectTableComponent {
     };
   }
   getProjects() {
-    return this.dashboardService.getProjects().subscribe(data => {
-      this.projects = data;
+    return this.dashboardService.getProjects().subscribe(downloaded => {
+      this.projects = downloaded;
       this.loading = false;
     });
   }
   deleteProject(id) {
     return this.dashboardService.deleteProject(id).subscribe(
-      data => {
+      downloaded => {
         this.toast.showToast('success', this.constants.DASHBOARD_TOAST_TITLE_SUCCESS,
           this.constants.DASHBOARD_OPERATION_PROJECT_DELETE_SUCCESS);
       },
@@ -128,7 +143,7 @@ export class ProjectTableComponent {
   }
   createProject(name, description, ciid, enableVulnManage) {
     return this.dashboardService.addProject(name, description, ciid, enableVulnManage).subscribe(
-      data => {
+      downloaded => {
         this.toast.showToast('success', this.constants.DASHBOARD_TOAST_TITLE_SUCCESS,
           this.constants.DASHBOARD_OPERATION_PROJECT_ADD_SUCCESS + name);
       },
@@ -139,7 +154,7 @@ export class ProjectTableComponent {
   }
   editProject(id, project) {
     return this.dashboardService.editProject(id, project).subscribe(
-      data => {
+      downloaded => {
         this.toast.showToast('success', this.constants.DASHBOARD_TOAST_TITLE_SUCCESS,
           this.constants.DASHBOARD_OPERATION_PROJECT_EDIT_SUCCESS + project.name);
         this.getProjects();
@@ -196,5 +211,30 @@ export class ProjectTableComponent {
   }
   delay(ms: number) {
     return new Promise( resolve => setTimeout(resolve, ms) );
+  }
+
+  open(dialog: TemplateRef<any>, event) {
+    const dataToModal = {
+      event: event.data,
+      projects: this.projects,
+    };
+    this.dialogService.open(
+      dialog,
+      { context: dataToModal });
+  }
+
+  saveMerge(value, ref: any) {
+    return this.dashboardService.moergeProjects(value.source, value.destination).subscribe(
+      downloaded => {
+        this.toast.showToast('success', this.constants.DASHBOARD_TOAST_TITLE_SUCCESS,
+          'Successfully merged selected projects.');
+        this.getProjects();
+        ref.close();
+      },
+      error => {
+        this.toast.showToast('danger', this.constants.DASHBOARD_TOAST_TITLE_FAILURE,
+          'Problem during merging selected projects.');
+        ref.close();
+      });
   }
 }
